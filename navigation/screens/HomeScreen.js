6b2173e70bc17eb3995dcbbd0e4781b54bb47a40
Jheanster <jctoledo935@gmail.com>
2023-10-16 15:15:04 -0400

    Animation updates
    Updated some animations on the home screen. You can now swipe a profile away which will rotate it. Behind the current profile the next one will appear slowly into view

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,22 +1,51 @@
 import 'react-native-gesture-handler'
 import React, { useState } from 'react'
-import { Pressable, StyleSheet, View, Text} from 'react-native'
-import Animated, { useSharedValue, useAnimatedStyle, withSpring, useAnimatedGestureHandler } from 'react-native-reanimated'
+import { Pressable, StyleSheet, View, Text, useWindowDimensions} from 'react-native'
+import Animated, { useSharedValue, useAnimatedStyle, withSpring, useAnimatedGestureHandler, useDerivedValue, interpolate } from 'react-native-reanimated'
 import { GestureHandlerRootView, PanGestureHandler } from 'react-native-gesture-handler'
 import Card from '../components/SpotMeCard'
 import users from '../users'
 
+
+const ROTATION = 60;
+
 function HomeScreen() {
 
-    const translateX = useSharedValue(0);
+    // Set the current user in the stack of cards
+    const [currentIndex,setCurrentIndex] = useState(0);
+    const currentProfile = users[currentIndex];
+
+    const[nextIndex, setNextIndex] = useState(currentIndex + 1);
+    const nextProfile = users[nextIndex];
+
+
+    const { width: screenWidth } = useWindowDimensions();
+    const hiddenTranslateX = 2 * screenWidth;
+
+
+    const translateX = useSharedValue(0);         // Translation values (from left, middle, right): -width 0 width
+    const rotate = useDerivedValue(() => interpolate(translateX.value, [0,hiddenTranslateX], [0,ROTATION]) + 'deg'); // Rotation values (from left, middle, right): -60deg 0 60deg 
+
+    // The current cards animation settings
     const cardStyle = useAnimatedStyle(() => ({
         transform: [
             {
                 translateX: translateX.value,
             },
             {
-                rotate: '0deg',
+                rotate: rotate.value,
             }
+    ],
+
+    }));
+
+    const nextCardStyle = useAnimatedStyle(() => ({
+        transform: [
+            {
+                scale: interpolate(translateX.value, 
+                    [-hiddenTranslateX,0,hiddenTranslateX],
+                    [1,0.5,1]),
+            },
     ],
 
     }));
@@ -28,21 +57,28 @@ function HomeScreen() {
 
         onActive: (event,context) => {
             translateX.value = context.startX + event.translationX;
-            console.log('Touch x: ', event.translationX);
+            // console.log('Touch x: ', event.translationX);
         },
 
         onEnd: () => {
-            console.log('Touch ended');
+            // console.log('Touch ended');
         }
     });
 
     return (
         // View for the first page
         <GestureHandlerRootView style={styles.container}>
+            <View style={styles.nextCardContainer}>
+                <Animated.View style={[styles.animatedCard,nextCardStyle]}>
+                    <Card user={nextProfile}/>
+                </Animated.View>
+            </View>
+           
+        
             <PanGestureHandler onGestureEvent={gestureHandler}>
                 <Animated.View style={[styles.animatedCard,cardStyle]}>
                     {/* Each card is a different profile. The general card layout can be changed in navigation/components/SpotMeCard/index.js*/}
-                    <Card user={users[2]}/>
+                    <Card user={currentProfile}/>
                 </Animated.View>
             </PanGestureHandler>
         </GestureHandlerRootView>
@@ -63,6 +99,15 @@ const styles = StyleSheet.create({
         width:'100%',
         justifyContent: 'center',
         alignItems: 'center',
+        flex: 1,
+    },
+
+    nextCardContainer:{
+        ...StyleSheet.absoluteFillObject,
+        width:'100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    
     },
 })
 
