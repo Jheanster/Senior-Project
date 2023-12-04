850e3d89b295ba0107f00c1ef03d62b57aedd160
jimmyngo4 <105337446+jimmyngo4@users.noreply.github.com>
2023-12-04 04:50:21 -0500

    formatting using Prettier

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,13 +1,25 @@
-import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
-import { useNavigation } from '@react-navigation/core'
-import { View, Text, Button, TouchableOpacity, Image, StyleSheet} from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { addProspectApprovalToDB, addProspectRejectionToDB, getLocalUserData, getProspectsData } from '../../backend/UserDBService';
-import { Ionicons, AntDesign, Entypo} from '@expo/vector-icons';
-import Swiper from "react-native-deck-swiper"
-import tw from 'twrnc';
-import { docDB } from '../../firebase';
-import generateId from '../../lib/generateId';
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { useNavigation } from "@react-navigation/core";
+import {
+  View,
+  Text,
+  Button,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  addProspectApprovalToDB,
+  addProspectRejectionToDB,
+  getLocalUserData,
+  getProspectsData,
+} from "../../backend/UserDBService";
+import { Ionicons, AntDesign, Entypo } from "@expo/vector-icons";
+import Swiper from "react-native-deck-swiper";
+import tw from "twrnc";
+import { docDB } from "../../firebase";
+import generateId from "../../lib/generateId";
 
 /*
     TODO:
@@ -19,142 +31,163 @@ import generateId from '../../lib/generateId';
 */
 
 function HomeScreen() {
-    const navigation = useNavigation();
-    const localUser = getLocalUserData();
-    const [profiles, setProfiles] = useState([]);
-    const swiperRef = useRef(null);
+  const navigation = useNavigation();
+  const localUser = getLocalUserData();
+  const [profiles, setProfiles] = useState([]);
+  const swiperRef = useRef(null);
 
-    useEffect(
-        () => setProfiles(getProspectsData()),
-        [localUser]
-    )
-    
-    const handleSwipeLeft = (cardIndex) => {
-        const userSwiped = profiles[cardIndex]
-        if(userSwiped){
-            console.log(`You rejected ${userSwiped.name}`)
-            addProspectRejectionToDB(userSwiped)
-        }
+  useEffect(() => setProfiles(getProspectsData()), [localUser]);
+
+  const handleSwipeLeft = (cardIndex) => {
+    const userSwiped = profiles[cardIndex];
+    if (userSwiped) {
+      console.log(`You rejected ${userSwiped.name}`);
+      addProspectRejectionToDB(userSwiped);
     }
+  };
 
-    const handleSwipeRight = (cardIndex) => {
-        const userSwiped = profiles[cardIndex]
-        if(userSwiped){
-            console.log(`You approved ${userSwiped.name}`)
+  const handleSwipeRight = (cardIndex) => {
+    const userSwiped = profiles[cardIndex];
+    if (userSwiped) {
+      console.log(`You approved ${userSwiped.name}`);
 
-            addProspectApprovalToDB(userSwiped, () => {
-                navigation.navigate("Match", {localUser, userSwiped});
-            })
-        }
+      addProspectApprovalToDB(userSwiped, () => {
+        navigation.navigate("Match", { localUser, userSwiped });
+      });
     }
+  };
 
-    return (
+  return (
     <SafeAreaView style={tw`flex-1`}>
-        {/* Header */}
-            <View style={tw`items-center relative`}>
-                <TouchableOpacity style={tw`absolute left-5 top-3`} onPress={() => navigation.navigate("Edit")}>
-                    <Image source={localUser.pfp ? {uri: localUser.pfp} : null} style={tw`h-10 w-10 rounded-full`} />
-                </TouchableOpacity>
+      {/* Header */}
+      <View style={tw`items-center relative`}>
+        <TouchableOpacity
+          style={tw`absolute left-5 top-3`}
+          onPress={() => navigation.navigate("Edit")}
+        >
+          <Image
+            source={localUser.pfp ? { uri: localUser.pfp } : null}
+            style={tw`h-10 w-10 rounded-full`}
+          />
+        </TouchableOpacity>
 
-                <TouchableOpacity onPress={() => navigation.navigate("Modal")}>
-                    <Image style={tw`h-14 w-14`} source={require("../../assets/images/dumbbell.png")}/>
-                </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Modal")}>
+          <Image
+            style={tw`h-14 w-14`}
+            source={require("../../assets/images/dumbbell.png")}
+          />
+        </TouchableOpacity>
 
-                <TouchableOpacity style={tw`absolute right-5 top-3`} onPress={() => navigation.navigate("Chat")}>
-                    <Ionicons name='chatbubbles-sharp' size={30}/>
-                </TouchableOpacity>
-            </View>
-        {/* End of Header */}
+        <TouchableOpacity
+          style={tw`absolute right-5 top-3`}
+          onPress={() => navigation.navigate("Chat")}
+        >
+          <Ionicons name="chatbubbles-sharp" size={30} />
+        </TouchableOpacity>
+      </View>
+      {/* End of Header */}
 
-        {/* Cards */}
-        <View style={tw`flex-1 -mt-6`}>
-            <Swiper
-            ref={swiperRef}
-            containerStyle={{backgroundColor:'transparent'}}
-                cards={profiles}
-                stackSize={5}
-                cardIndex={0}
-                animateCardOpacity
-                verticalSwipe={false}
-                onSwipedLeft={handleSwipeLeft}
-                onSwipedRight={handleSwipeRight}
-                backgroundColor='#4FD0E9'
-                overlayLabels={{
-                    left: {
-                        title: "Not Today",
-                        style: {
-                            label: {
-                                textAlign: 'right',
-                                color: 'red',
-                            }
-                        }
-                    },
-                    right: {
-                        title: "Lets get it",
-                        style: {
-                            label: {
-                                textAlign: 'left',
-                                color: '#4DED30',
-                            }
-                        }
-                    }
-                }}
-                renderCard={card => card ? (
-                    <View key={card.id} style={tw`relative bg-white h-3/4 rounded-xl`}>
-                        <Image 
-                            style={tw`absolute top-0 h-full w-full rounded-xl`} 
-                            source={card.pfp ? {uri: card.pfp} : null}
-                        />
+      {/* Cards */}
+      <View style={tw`flex-1 -mt-6`}>
+        <Swiper
+          ref={swiperRef}
+          containerStyle={{ backgroundColor: "transparent" }}
+          cards={profiles}
+          stackSize={5}
+          cardIndex={0}
+          animateCardOpacity
+          verticalSwipe={false}
+          onSwipedLeft={handleSwipeLeft}
+          onSwipedRight={handleSwipeRight}
+          backgroundColor="#4FD0E9"
+          overlayLabels={{
+            left: {
+              title: "Not Today",
+              style: {
+                label: {
+                  textAlign: "right",
+                  color: "red",
+                },
+              },
+            },
+            right: {
+              title: "Lets get it",
+              style: {
+                label: {
+                  textAlign: "left",
+                  color: "#4DED30",
+                },
+              },
+            },
+          }}
+          renderCard={(card) =>
+            card ? (
+              <View
+                key={card.id}
+                style={tw`relative bg-white h-3/4 rounded-xl`}
+              >
+                <Image
+                  style={tw`absolute top-0 h-full w-full rounded-xl`}
+                  source={card.pfp ? { uri: card.pfp } : null}
+                />
 
-                        <View style={[tw`absolute bottom-0 bg-white flex-row justify-between items-center w-full h-20 px-6 py-2 rounded-b-xl`,styles.cardShadow]}>
-                            <View>
-                                <Text style={tw`text-xl font-bold`}>{card.name}</Text>
-                                <Text>{card.bio}</Text>
-                            </View>
-                            <Text style={tw`text-2xl font-bold`}>{card.age}</Text>
-                        </View>
+                <View
+                  style={[
+                    tw`absolute bottom-0 bg-white flex-row justify-between items-center w-full h-20 px-6 py-2 rounded-b-xl`,
+                    styles.cardShadow,
+                  ]}
+                >
+                  <View>
+                    <Text style={tw`text-xl font-bold`}>{card.name}</Text>
+                    <Text>{card.bio}</Text>
+                  </View>
+                  <Text style={tw`text-2xl font-bold`}>{card.age}</Text>
+                </View>
+              </View>
+            ) : (
+              <View
+                style={[
+                  tw`relative bg-white h-3/4 rounded-xl justify-center items-center`,
+                  styles.cardShadow,
+                ]}
+              >
+                <Text style={tw`font-bold pb-5`}>No more profiles</Text>
+              </View>
+            )
+          }
+        />
+      </View>
 
-                    </View>
-                ) : (
-                    <View style={[tw`relative bg-white h-3/4 rounded-xl justify-center items-center`, styles.cardShadow]}>
-                        <Text style={tw`font-bold pb-5`}>
-                            No more profiles
-                        </Text>
-                    </View>
-                )}
-            />
-        </View>
-        
-        <View style={tw`flex flex-row justify-evenly`}>
-            <TouchableOpacity style={tw`items-center justify-center rounded-full w-16 h-16 bg-red-200`}
-                onPress={() => swiperRef.current.swipeLeft()}
-            >
-                <Entypo name='cross' size={24} color={'red'}/>
-            </TouchableOpacity>
-            <TouchableOpacity style={tw`items-center justify-center rounded-full w-16 h-16 bg-green-200`} 
-                onPress={() => swiperRef.current.swipeRight()}
-            >
-                <AntDesign name='heart' size={24} color={'green'}/>
-            </TouchableOpacity>
-        </View>
-
+      <View style={tw`flex flex-row justify-evenly`}>
+        <TouchableOpacity
+          style={tw`items-center justify-center rounded-full w-16 h-16 bg-red-200`}
+          onPress={() => swiperRef.current.swipeLeft()}
+        >
+          <Entypo name="cross" size={24} color={"red"} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={tw`items-center justify-center rounded-full w-16 h-16 bg-green-200`}
+          onPress={() => swiperRef.current.swipeRight()}
+        >
+          <AntDesign name="heart" size={24} color={"green"} />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
-    )
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
 
 const styles = StyleSheet.create({
-
-    cardShadow: {
-        // Literally search up 'react native shadow generator', gives a slider to change how the shadow looks
-        shadowColor: "#000",
-        shadowOffset: {
-	        width: 0,
-	        height: 3,
-        },
-        shadowOpacity: 0.29,
-        shadowRadius: 4.65,
-        elevation: 2,
+  cardShadow: {
+    // Literally search up 'react native shadow generator', gives a slider to change how the shadow looks
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
     },
-})
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+    elevation: 2,
+  },
+});
